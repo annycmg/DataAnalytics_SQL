d58b1a2421b1e8b2971412ea297f1f58005b6ea8
annycmg <38812492+annycmg@users.noreply.github.com>
2021-09-10 09:28:40 -0300

    Update Intermidiate.sql

--- Intermidiate.sql
+++ Intermidiate.sql
@@ -30,4 +30,42 @@ FROM Table1
 GROUP BY CASE WHEN Column1 = 'FR' THEN 'FR'
               ELSE 'No Year Data' END
                
--- JOINS          
+-- INNER JOIN (return matched values between the columns.)
+SELECT A.Column1 AS First_Column, 
+       B.Column10 AS Tenth_Column
+FROM Table1 A
+JOIN Table2 B
+ON A.First_Column = B.Tenth_Column
+
+-- LEFT/RIGHT JOIN (return all rows on left/right side.)
+SELECT A.Column1 AS First_Column,
+       B.Column10 AS Tenth_Column
+FROM Table1 A
+LEFT JOIN Table2 B -- or RIGHT JOIN
+ON A.First_Column = B.Tenth_Column
+
+-- FULL JOIN (return all rows from left and right side.)
+SELECT A.Column1 AS First_Column,
+       B.Column10 AS Tenth_Column
+FROM Table1 A
+FULL OUTER JOIN Table2 B
+ON A.First_Column = B.Tenth_Column
+
+-- CHAINED JOIN
+SELECT A.Column1 AS First_Column, 
+       Column10 AS Tenth_Column, 
+       Column20 AS Twenieth_Column
+FROM Table1 AS A
+INNER JOIN Table2 AS B
+ON A.First_Columns = B.Tenth_Column
+INNER JOIN Table3 AS C
+ON A.First_Column = C.Twenieth_Column
+
+-- JOIN + WHERE + GROUP BY
+SELECT A.Column1 AS First_Column, 
+       B.Column10 AS Tenth_Column
+FROM Table1 A
+LEFT JOIN Table2 B
+ON A.First_Column = B.Tenth_Column
+WHERE B.Tenth_Column > A.First_Column + 5
+GROUP BY A.First_Column
